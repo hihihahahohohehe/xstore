4a751267acb25eead9e0ecf43555bdcac0d38276
Nhi Dao <hihihahahohohehe@gmail.com>
2013-05-11 19:48:40 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Xstore::Application.config.secret_token = '878ec4c3cf2b6596a139d77648a9e2a4f7727faea21f44ad245adbbaffc276b9a025e4de913dc32b4919f907f76a2722f10818782fa7e059dd5b3c8bc492b7a9'
